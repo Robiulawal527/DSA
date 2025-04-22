7c9d007552fbe418fae45cc6f904dc97d96e6f2b
Robiul Awal <robiulawal527@gmail.com>
2025-04-22 06:17:06 +0600

    binary search

--- dsaMidSpring25/binarySearch.c
+++ dsaMidSpring25/binarySearch.c
@@ -9,7 +9,9 @@ int main()
 
     int size = sizeof(A) / sizeof(A[0]);
 
-    binarySearch(A, size);
+    int result = binarySearch(A, size);
+
+    printf("%d key is found /n", A[result]);
 
     return 0;
 }
@@ -17,22 +19,29 @@ int main()
 int binarySearch(int A[], int size)
 {
 
-    int key = 50;
+    int key = 10;
 
     int low = 0;
-    int high = size -1; 
+    int high = size - 1;
 
-    while (low <= high){
+    while (low <= high)
+    {
 
         int mid = (low + high) / 2;
 
-        if (A[mid] == key){
-            
+        if (A[mid] == key)
+        {
+            return mid;
         }
-        
-
+        else if (A[mid] > key)
+        {
+            high = mid - 1;
+        }
+        else if (A[mid] < key)
+        {
+            low = mid + 1;
+        }
     }
-
 
     return 0;
 }
