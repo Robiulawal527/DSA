c77dea8c766a802bdbc83381100f3a50d03d25e1
Robiul Awal <robiulawal527@gmail.com>
2025-11-21 22:45:32 +0600

    hello recursion

---
+++ DSA_2/Recursion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    printf("Hello world");
+    
+    return 0;
+}
