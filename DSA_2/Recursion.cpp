f9f20744b7f69f81fac6ac7233ec582e049601ff
Robiul Awal <robiulawal527@gmail.com>
2025-11-22 11:46:12 +0600

    Improve code readability by adding comments in Recursion.cpp

--- DSA_2/Recursion.cpp
+++ DSA_2/Recursion.cpp
@@ -16,12 +16,19 @@ int main() {
 
     int arr[n];
 
+    // set seed time to null 
     srand(time(NULL));
 
     for(int i=0; i<n; i++){
+
+        // generate random number each iteration.
         int x = rand() % 100;
+
+        // asign to the array's i th index
         arr[i] = x;
     }
+
+    // printing the array in one line
 
     for(int i=0; i<n; i++) cout << arr[i] <<" ";
 
