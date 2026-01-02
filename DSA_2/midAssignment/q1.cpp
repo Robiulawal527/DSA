d4a203e1ae03550b34bef7b82bbb349af99ca798
Robiul Awal <robiulawal527@gmail.com>
2026-01-02 20:20:42 +0600

    Add initial implementation for array input and display in q1.cpp

---
+++ DSA_2/midAssignment/q1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+
+    int n; 
+    cout << "Enter array size : "; 
+    cin >> n; 
+
+    vector <long long> arr(n);
+
+    cout << "Enter array elements: ";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    cout << "Array Elements are: ";
+    for(int i=0; i<n; i++){
+        cout << " " << arr[i];
+    }
+
+
+
+    
+
+
+
+    
+    
+    return 0;
+}
