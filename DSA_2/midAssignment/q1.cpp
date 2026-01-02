6e5a86ce91b0a8cec5e736be6308e832ba4796b5
Robiul Awal <robiulawal527@gmail.com>
2026-01-02 23:08:28 +0600

    feat: Implement max product subarray function using Kadane's algorithm

--- DSA_2/midAssignment/q1.cpp
+++ DSA_2/midAssignment/q1.cpp
@@ -4,6 +4,26 @@
 
 using namespace std;
 
+int maxProductKadane(const vector<long long>& arr) {
+    int n = arr.size();
+    if (n == 0) return 0;
+
+    int global_max = arr[0];
+    int curr_max = arr[0];
+    int curr_min = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        int temp = curr_max;
+
+        curr_max = max({arr[i], temp * arr[i], curr_min * arr[i]});
+        curr_min = min({arr[i], temp * arr[i], curr_min * arr[i]});
+
+        global_max = max(global_max, curr_max);
+    }
+
+    return global_max;
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -26,6 +46,10 @@ int main() {
         cout << " " << arr[i];
     }
 
+    cout << " " << endl; 
+
+
+    cout << "Maximum Product: " << maxProductKadane(arr) << endl;
 
 
     return 0;
