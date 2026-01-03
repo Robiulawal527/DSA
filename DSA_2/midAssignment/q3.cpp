88ba92f443ff3b0d0f5f329249d75aa9487f3b6c
Robiul Awal <robiulawal527@gmail.com>
2026-01-03 17:32:24 +0600

    refactor: Rename variables for clarity and consistency in q3.cpp

--- DSA_2/midAssignment/q3.cpp
+++ DSA_2/midAssignment/q3.cpp
@@ -9,35 +9,36 @@ int main() {
     int n;
         cin >> n;
         
-        vector<long long> freq(n);
-        long long total_freq = 0;
+        vector<long long> freqCount(n);
+        
+        long long total_freqCount = 0;
         for (int i = 0; i < n; i++) {
-            cin >> freq[i];
-            total_freq += freq[i];
+            cin >> freqCount[i];
+            total_freqCount += freqCount[i];
         }
         
         if (n <= 1) {
             cout << 0 << endl;
         }
-        
+
         priority_queue<long long, vector<long long>, greater<long long>> pq;
-        for (auto f : freq) {
+        for (auto f : freqCount) {
             pq.push(f);
         }
         
-        long long total_bits = 0;
+        long long totalBitsNumber = 0;
         
         while (pq.size() > 1) {
             long long a = pq.top(); pq.pop();
             long long b = pq.top(); pq.pop();
             
             long long merge = a + b;
-            total_bits += merge;
+            totalBitsNumber += merge;
             
             pq.push(merge);
         }
         
-        cout << total_bits << endl;
+        cout << totalBitsNumber << endl;
     
     return 0;
 }
