3a821cb52a21b805cc568e1158d49b21bf8549c2
Robiul Awal <robiulawal527@gmail.com>
2026-02-04 00:44:04 +0600

    feat: Implement basic structure for Disjoint Set with makeSet, find, and unite functions

--- DSA_2/disjointSet.cpp
+++ DSA_2/disjointSet.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <vector>
-#include <string>   
+#include <vector> 
 
 using namespace std;
+
+int makeSet(){
+    return 0;
+}
+
+int find(int v){
+    return 0;
+}
+
+bool unite(int a,int b){
+    return false;
+}
 
 
 
 int main(){
+
+    
     
 
     return 0;
